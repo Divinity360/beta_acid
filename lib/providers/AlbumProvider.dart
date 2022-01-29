9588b50239baa54917c7195cdc280611708361a2
divinity360 <thedivinitysoft@gmail.com>
2022-01-29 22:39:54 +0100

    setup api service

--- lib/providers/AlbumProvider.dart
+++ lib/providers/AlbumProvider.dart
@@ -1,5 +1,17 @@
+import 'package:beta_acid/models/Album.dart';
+import 'package:beta_acid/services/ApiService.dart';
 import 'package:flutter/material.dart';
 
 class AlbumProvider extends ChangeNotifier {
+  late List<Album> _albumsList;
 
+  List<Album> get albumsList => _albumsList;
+
+  Future fetchAlbumsList() async {
+    var response = await ApiService.fetchAlbums();
+    if (response != null) {
+      _albumsList = response;
+    }
+    notifyListeners();
+  }
 }
--- lib/services/ApiService.dart
+++ lib/services/ApiService.dart
@@ -6,5 +6,13 @@ import 'package:beta_acid/models/Album.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-
+  static Future<List<Album>?> fetchAlbums() async {
+    var response = await http.get(Uri.parse(Constants.ALBUM_LOOKUP_URL));
+    log(response.body);
+    if (response.statusCode == HttpStatus.ok) {
+      final results = json.decode(response.body)["results"];
+      return results.map<Album>((item) => Album.fromJson(item)).toList();
+    }
+    return null;
+  }
 }
